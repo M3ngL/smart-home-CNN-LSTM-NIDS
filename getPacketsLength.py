1902d46c8d033ce40e9ae73a9da490bbce171dad
M3ng@L <88039157+M3ngL@users.noreply.github.com>
2024-05-23 12:34:20 +0800

    Delete getPacketsLength.py

--- getPacketsLength.py
+++
@@ -1,7 +0,0 @@
-def getPacketsLength(flow_key, flows, packets_length):
-    for i in range(len(flows[flow_key])):
-        data_packet = flows[flow_key][i][1]
-        packets_length.append(len(data_packet))
-
-    return packets_length
-
